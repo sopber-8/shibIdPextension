df764557bc0dcddd615fc6e51171f4493e14e8ca
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2012-05-10 14:23:24 +0200

    Added .gitignore and made some changes to Namespace in the BeanDefinitionFactory

--- src/main/java/se/danetest/shibboleth/extension/DaneSignatureTrustEngineNamespaceHandler.java
+++ src/main/java/se/danetest/shibboleth/extension/DaneSignatureTrustEngineNamespaceHandler.java
@@ -8,11 +8,11 @@ public class DaneSignatureTrustEngineNamespaceHandler extends BaseSpringNamespac
 	
 	private final Logger log = LoggerFactory.getLogger(DaneSignatureTrustEngineNamespaceHandler.class);
 
-	public static final String NAMESPACE = "urn:mace:shibboleth:danetrustengine";
+	public static final String NAMESPACE = "urn:mace:danetest:danetrustengine";
 
     public void init() {
-        registerBeanDefinitionParser(DaneSignatureEngineBeanDefinitionParser.SCHEMA_TYPE, new DaneSignatureEngineBeanDefinitionParser());
-        // other calls to registerBeanDefinitionParser for other extensions in this namespace would go here
-        log.debug("public void init(), registerBeanDefinitionParser(DaneSignatureEngineBeanDefinitionParser.SCHEMA_TYPE, new DaneSignatureEngineBeanDefinitionParser());");
+    	log.debug("public void init(), registerBeanDefinitionParser(DaneSignatureEngineBeanDefinitionParser.SCHEMA_TYPE, new DaneSignatureEngineBeanDefinitionParser());");
+    	registerBeanDefinitionParser(DaneSignatureEngineBeanDefinitionParser.SCHEMA_TYPE, new DaneSignatureEngineBeanDefinitionParser());
+        // other calls to registerBeanDefinitionParser for other extensions in this namespace would go here  
     }
 }
